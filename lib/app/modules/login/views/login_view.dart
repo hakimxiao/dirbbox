8d28cc418e49a375b0bc99fc468f87807e7df16f
hakimxiao <alhakimabusolihin@gmail.com>
2025-11-06 15:03:33 +0700

    - Melakukkan Desain Pada Login View Dan Meletakkan Beberapa Assets -

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -9,15 +9,50 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          // background
+          SizedBox(
+            width: Get.width,
+            child: Image.asset("assets/images/bg.png", fit: BoxFit.cover),
+          ),
+          // layer body
+          Column(
+            children: [
+              SizedBox(height: 135),
+              SizedBox(
+                width: 200,
+                height: 200,
+                child: Image.asset(
+                  "assets/images/pic-1.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
+              Text("Welcome To"),
+              Text("Dirbbox"),
+              SizedBox(
+                width: 250,
+                child: Text(
+                  "Best cloud storage platform for all business and individuals to manage there data\n\n Join For Free.",
+                ),
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF567DF4).withOpacity(0.4),
+                    ),
+                    child: Row(
+                      children: [Image.asset("assets/icons/finger.png")],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
