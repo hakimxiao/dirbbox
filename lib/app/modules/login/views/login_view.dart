83ff142a2b29c2a3f79e15a47e14cc53df301f80
hakimxiao <alhakimabusolihin@gmail.com>
2025-11-07 15:02:07 +0700

    - MENYELESAIKAN UI UNTUK LOGIN PAGE DAN MEMBUAT PAGE BARU BERNAMA PROFILE -

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -18,39 +18,109 @@ class LoginView extends GetView<LoginController> {
             child: Image.asset("assets/images/bg.png", fit: BoxFit.cover),
           ),
           // layer body
-          Column(
-            children: [
-              SizedBox(height: 135),
-              SizedBox(
-                width: 200,
-                height: 200,
-                child: Image.asset(
-                  "assets/images/pic-1.png",
-                  fit: BoxFit.contain,
-                ),
-              ),
-              Text("Welcome To"),
-              Text("Dirbbox"),
-              SizedBox(
-                width: 250,
-                child: Text(
-                  "Best cloud storage platform for all business and individuals to manage there data\n\n Join For Free.",
-                ),
-              ),
-              Row(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFF567DF4).withOpacity(0.4),
-                    ),
-                    child: Row(
-                      children: [Image.asset("assets/icons/finger.png")],
-                    ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 35),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 135),
+                SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Image.asset(
+                    "assets/images/pic-1.png",
+                    fit: BoxFit.contain,
                   ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(height: 20),
+                Text("Welcome To", style: TextStyle(fontSize: 22)),
+                Text(
+                  "Dirbbox",
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  width: 250,
+                  child: Text(
+                    "Best cloud storage platform for all business and individuals to manage there data\n\n Join For Free.",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFEEF7FE),
+                        fixedSize: Size(150, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset("assets/icons/finger.png"),
+                          SizedBox(width: 10),
+                          Text(
+                            "Smart ID",
+                            style: TextStyle(color: Color(0xFF567DF4)),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF567DF4),
+                        fixedSize: Size(150, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Sign in",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(width: 10),
+                          Image.asset("assets/icons/panah-kanan.png"),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 40),
+                Center(
+                  child: Text(
+                    "Use Social Login",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/icons/ig.png"),
+                    SizedBox(width: 60),
+                    Image.asset("assets/icons/twt.png"),
+                    SizedBox(width: 60),
+                    Image.asset("assets/icons/fb.png"),
+                  ],
+                ),
+                SizedBox(height: 50),
+                Center(
+                  child: Text(
+                    "Create an account",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
